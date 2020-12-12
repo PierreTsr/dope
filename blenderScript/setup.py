526ed2fac901d141a754b869320d0e5994a6c74e
3rdCore <tom.marty@polytechnique.edu>
2020-12-12 21:19:47 +0100

    modified script

--- blenderScript/setup.py
+++ blenderScript/setup.py
@@ -2,26 +2,26 @@ import bpy
 
 
 class OBJECT_MT_Panel(bpy.types.WorkSpaceTool):
-	"""Creates a Panel in the Object properties window"""
-	bl_label = "OpenCV Animation"
-	bl_space_type = 'VIEW_3D'
-	bl_context_mode = 'OBJECT'
-	bl_idname = "ui_plus.opencv"
-	bl_options = {'REGISTER'}
-	bl_icon = "ops.generic.select_circle"
+    """Creates a Panel in the Object properties window"""
+    bl_label = "OpenCV Animation"
+    bl_space_type = 'VIEW_3D'
+    bl_context_mode = 'OBJECT'
+    bl_idname = "ui_plus.opencv"
+    bl_options = {'REGISTER'}
+    bl_icon = "ops.generic.select_circle"
 
-	def draw_settings(context, layout, tool):
-		row = layout.row()
-		op = row.operator("wm.rigging", text="Capture", icon="OUTLINER_OB_CAMERA")
+    def draw_settings(context, layout, tool):
+        row = layout.row()
+        op = row.operator("wm.rigging", text="Capture", icon="OUTLINER_OB_CAMERA")
 
 
 def register():
-	bpy.utils.register_tool(OBJECT_MT_Panel, separator=True, group=True)
+    bpy.utils.register_tool(OBJECT_MT_Panel, separator=True, group=True)
 
 
 def unregister():
-	bpy.utils.unregister_tool(OBJECT_MT_Panel)
+    bpy.utils.unregister_tool(OBJECT_MT_Panel)
 
 
 if __name__ == "__main__":
-	register()
+    register()
